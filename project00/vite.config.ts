cd605ad148276894d5b16809fbc629feba02c43b
amber2713 <2713033233@qq.com>
2026-01-26 00:43:42 +0800

    Update vite.config.ts

--- project00/vite.config.ts
+++ project00/vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 部署到 https://amber2713.github.io/wangzijian/ 时，必须配置此项
+  base: '/wangzijian/', 
+  
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  
+  build: {
+    // 确保打包输出目录正确（默认就是 dist）
+    outDir: 'dist',
+    // 资源存放的文件夹
+    assetsDir: 'assets',
   },
-});
+})
