1648798be116f56cdefc10242fb3ff73518a36ee
amber2713 <2713033233@qq.com>
2026-01-26 08:04:06 +0800

    Update base path for project deployment

--- project00/vite.config.ts
+++ project00/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // 部署到 https://amber2713.github.io/wangzijian/ 时，必须配置此项
-  base: '/wangzijian/', 
+  base: '/wangzijian/project00/', 
   
   plugins: [react()],
   
